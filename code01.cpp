0826bbfe61f2c0a076ab609a818b44293a3399ce
edzordzinam44 <edzordzinam.alipui@gmail.com>
2026-06-22 23:30:03 -1200

    Add basic calculator program for summing two numbers

---
+++ code01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double num1,num2,result;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    result=num1+num2;
+
+    cout<<"Your result is: "<<result;
+    return 0;
+}
