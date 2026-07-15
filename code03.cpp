b207e1c90d7e48b3d1c409e3f273283c63d303f9
edzordzinam44 <edzordzinam.alipui@gmail.com>
2026-07-14 22:55:24 -1200

    Add initial implementation of sum function and main program

---
+++ code03.cpp
@@ -0,0 +1,21 @@
+//Function with Argument
+#include <iostream>
+using namespace std;
+
+double sum(double a, double b)
+{
+	double result;
+	result=a+b;
+	return result;
+}
+
+int main()
+{
+	double k;
+	double x=-2;
+	double y;
+	k=sum(7,40);
+	cout<<y;
+
+	return 0;
+}
