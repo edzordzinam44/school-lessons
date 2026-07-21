f755970ace94e461d251ed4ae241bcb9b1e8e3fc
edzordzinam44 <edzordzinam.alipui@gmail.com>
2026-07-20 22:33:37 -1200

    Add implementation of Vehicle class which is public and user input for vehicle details

---
+++ code10.cpp
@@ -0,0 +1,30 @@
+//Creating an object using a class which is private
+#include <iostream>
+using namespace std;
+
+struct Vehicle
+{
+	string name,model,color;
+	int yearofmanu;
+	double length;
+};
+
+int main()
+{
+	Vehicle bugatti,toyota,kia,opel,nissan,benz;
+	
+	cout<<"Enter vehicle name: ";
+	cin>>benz.name;
+	cout<<"Enter vehicle model: ";
+	cin>>benz.name;
+	cout<<"Enter vehicle year: ";
+	cin>>benz.yearofmanu;
+	cout<<"Enter vehicle color: ";
+	cin>>benz.color;
+	
+	cout<<"\nVehicle Name is: "<<benz.name;
+	cout<<"\nAnimal year is: "<<benz.yearofmanu;
+	cout<<"\nAnimal color is: "<<benz.color;
+	
+	return 0;
+}
